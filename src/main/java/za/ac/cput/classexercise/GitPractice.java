a2b7556bd92871208b5ace00a8a8517e08ddb6ce
Nolubabalo Ndongeni <219319464@mycput.ac.za>
2022-02-11 18:41:20 +0000

    Changes made into the GitPractice and GitPracticeTest files

--- src/main/java/za/ac/cput/classexercise/GitPractice.java
+++ src/main/java/za/ac/cput/classexercise/GitPractice.java
@@ -5,5 +5,14 @@ package za.ac.cput.classexercise;
  */
 public class GitPractice
 {
+    public int subtraction(String num1, String num2, String num3){
 
+        int val1 = Integer.parseInt(num1);
+        int val2 = Integer.parseInt(num2);
+        int val3 = Integer.parseInt(num3);
+        val3 = val1;
+
+
+        return val1 - val2 - val3;
+    }
 }
